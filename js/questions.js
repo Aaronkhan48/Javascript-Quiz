1a7b6a75769b968a23aa492331356cccd0303987
Aaronkhan48 <72841968+Aaronkhan48@users.noreply.github.com>
2023-05-24 12:24:46 -0400

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented JavaScript?",
+    answer: "Brendan Eich",
+    options: [
+      "Douglas Crockford",
+      "Sheryl Sandberg",
+      "Brendan Eich",
+      "Mike 'One Mic' Gallagher"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of these is a JavaScript package manager?",
+    answer: "npm",
+    options: [
+      "Node.js",
+      "TypeScript",
+      "npm",
+      "vanilla"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which tool can you use to ensure code quality?",
+    answer: "ESLint",
+    options: [
+      "Angular",
+      "jQuery",
+      "RequireJS",
+      "ESLint"
+    ]
+  }
+];
